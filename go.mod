8284f82899c259c37909680e0ad9c2cf22b055eb
JIEEEN <tjtpp009@korea.ac.kr>
2024-02-26 15:51:46 +0900

    feat db init, connection

--- go.mod
+++ go.mod
@@ -2,7 +2,13 @@ module github.com/JIEEEN/grpc-board
 
 go 1.22.0
 
-require fyne.io/fyne/v2 v2.4.4
+require (
+	fyne.io/fyne/v2 v2.4.4
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/joho/godotenv v1.5.1
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.26.0
+)
 
 require (
 	fyne.io/systray v1.10.1-0.20231115130155-104f5ef7839e // indirect
@@ -17,29 +23,21 @@ require (
 	github.com/go-text/render v0.0.0-20230619120952-35bccb6164b8 // indirect
 	github.com/go-text/typesetting v0.1.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
-	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/tevino/abool v1.2.0 // indirect
-	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
-	github.com/xdg-go/scram v1.1.2 // indirect
-	github.com/xdg-go/stringprep v1.0.4 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	github.com/yuin/goldmark v1.5.5 // indirect
-	go.mongodb.org/mongo-driver v1.14.0 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/image v0.11.0 // indirect
 	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
 )
---
+++ server/db/db.go
@@ -0,0 +1,111 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	dbType := os.Getenv("DB_TYPE")
+	dbDetail := os.Getenv("DB_DETAIL")
+
+	var err error
+
+	DB, err = sql.Open(dbType, dbDetail)
+	if err != nil {
+		log.Fatalf("Cannot connect to DB: %v", err)
+	}
+	defer DB.Close()
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Cannot connect to DB: %v", err)
+	}
+
+	DB.SetConnMaxLifetime(time.Minute * 5)
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(10)
+	log.Printf("Connect success to db: %v", dbType)
+
+	dbName := createDB()
+	log.Printf("Success to create database: %s", dbName)
+
+	createTable()
+	log.Printf("Success to create tables")
+}
+
+func createDB() string {
+	dbName := os.Getenv("DB_DATABASE")
+	_, err := DB.Exec("create database if not exists " + dbName)
+	if err != nil {
+		log.Fatalf("Cannot create database %s: %v", dbName, err)
+	}
+
+	_, err = DB.Exec("use " + dbName)
+	if err != nil {
+		log.Fatalf("Cannot use database %s: %v", dbName, err)
+	}
+
+	return dbName
+}
+
+func createTable() {
+	_, err := DB.Exec(`
+		create table if not exists users
+		(
+			id int not null auto_increment primary key,
+			email varchar(255) unique not null,
+			password varchar(255) not null,
+			nickname varchar(50),
+			create_at timestamp default current_timestamp,
+			updated_at timestamp default current_timestamp on update current_timestamp,
+			deleted_at timestamp
+		)
+	`)
+	if err != nil {
+		log.Fatalf("Cannot create table users: %v", err)
+	}
+
+	_, err = DB.Exec(`
+		create table if not exists boards
+		(
+			id int not null auto_increment primary key,
+			user_id int,
+			nickname varchar(50) unique not null,
+			title varchar(255) not null,
+			contents text,
+			create_at timestamp default current_timestamp,
+			updated_at timestamp default current_timestamp on update current_timestamp,
+			deleted_at timestamp,
+			foreign key (user_id) references users(id)
+		)
+	`)
+	if err != nil {
+		log.Fatalf("Cannot create table boards: %v", err)
+	}
+
+	_, err = DB.Exec(`
+		create table if not exists replies
+		(
+			id int not null auto_increment primary key,
+			user_id int,
+			nickname varchar(255),
+			board_id int,
+			contents text,
+			create_at timestamp default current_timestamp,
+			updated_at timestamp default current_timestamp on update current_timestamp,
+			deleted_at timestamp,
+			foreign key (user_id) references users(id),
+			foreign key (board_id) references boards(id)
+		)
+	`)
+	if err != nil {
+		log.Fatalf("Cannot create table replies: %v", err)
+	}
+}
