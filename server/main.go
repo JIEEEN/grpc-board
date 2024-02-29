4f86fe391c2a40211ba820839669d9f644c8e3e1
JIEEEN <tjtpp009@korea.ac.kr>
2024-02-29 12:55:34 +0900

    mod env name: GRPC_PORT -> GRPC_SERVER_PORT

--- server/main.go
+++ server/main.go
@@ -9,6 +9,7 @@ import (
 	pb "github.com/JIEEEN/grpc-board/proto"
 	"google.golang.org/grpc"
 
+	"github.com/JIEEEN/grpc-board/server/board"
 	"github.com/JIEEEN/grpc-board/server/db"
 	"github.com/JIEEEN/grpc-board/server/env"
 	"github.com/JIEEEN/grpc-board/server/user"
@@ -23,15 +24,21 @@ func init() {
 
 func main() {
 	defer _db.Close()
-	port := os.Getenv("GRPC_PORT")
+	port := os.Getenv("GRPC_SERVER_PORT")
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Cannot connect to Server: %v", err)
 	}
 	s := grpc.NewServer()
 
+	// authServer := auth.NewServer()
+	boardServer := board.NewServer(_db)
+	// replyServer := reply.NewServer()
 	userServer := user.NewServer(_db)
 
+	// pb.RegisterAuthServiceServer(s, authServer)
+	pb.RegisterBoardServiceServer(s, boardServer)
+	// pb.RegisterReplyServiceServer(s, replyServer)
 	pb.RegisterUserServiceServer(s, userServer)
 
 	log.Printf("Server listening at %v", lis.Addr())
