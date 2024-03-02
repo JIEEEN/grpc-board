26833951471ab9f3bf30af6041935f4cc9d101e5
JIEEEN <tjtpp009@korea.ac.kr>
2024-03-02 14:35:57 +0900

    feat group_user: proto & service code

--- server/group/user.go
+++ server/group/user.go
@@ -10,20 +10,20 @@ import (
 )
 
 type userServer struct {
-	pb.UnimplementedBoardGroupServiceServer
+	pb.UnimplementedGroupUserServiceServer
 	db *sql.DB
 }
 
-func NewBoardServer(db *sql.DB) *userServer {
+func NewUserServer(db *sql.DB) *userServer {
 	return &userServer{
 		db: db,
 	}
 }
 
 func (s *userServer) GetAllGroupUsers(ctx context.Context, in *emptypb.Empty) (*pb.GroupUsersInfo, error) {
-	var groupUsers []*pb.GroupUser
+	groupUsers := []*pb.GroupUser{}
 
-	rows, err := s.db.Query("select * from group_users")
+	rows, err := s.db.Query("select id, group_id, user_id from group_users")
 	if err != nil {
 		log.Printf("Failed to execute query: %v", err)
 		return &pb.GroupUsersInfo{}, err
@@ -37,21 +37,83 @@ func (s *userServer) GetAllGroupUsers(ctx context.Context, in *emptypb.Empty) (*
 			log.Printf("Failed to get row data: %v", err)
 			return &pb.GroupUsersInfo{}, err
 		}
+
+		groupUser := &pb.GroupUser{
+			Id:      r_id,
+			GroupId: r_groupId,
+			UserId:  r_userId,
+		}
+
+		groupUsers = append(groupUsers, groupUser)
 	}
+
+	return &pb.GroupUsersInfo{GroupUsers: groupUsers}, nil
 }
 
 func (s *userServer) GetGroupUserId(ctx context.Context, in *pb.GetGroupUserRequest) (*pb.GetGroupUserResponse, error) {
+	id := in.GetId()
 
+	var r_id, r_groupId, r_userId string
+	err := s.db.QueryRow("select * from group_users where id = ?", id).Scan(&r_id, &r_groupId, &r_userId)
+	if err != nil {
+		log.Printf("Failed to execute query: %v", err)
+		return &pb.GetGroupUserResponse{}, err
+	}
+
+	groupUser := &pb.GroupUser{
+		Id:      r_id,
+		GroupId: r_groupId,
+		UserId:  r_userId,
+	}
+
+	return &pb.GetGroupUserResponse{GroupUser: groupUser}, nil
 }
 
-func (s *userServer) CreateGroupUser(ctx context.Context, in *pb.CreateBoardGroupRequest) (*pb.CreateBoardGroupResponse, error) {
+func (s *userServer) CreateGroupUser(ctx context.Context, in *pb.CreateGroupUserRequest) (*pb.CreateGroupUserResponse, error) {
+	groupUser := in.GetGroupUserRequest()
 
+	_, err := s.db.Exec("insert into group_users (group_id, user_id) values (?, ?)",
+		groupUser.GetGroupId(), groupUser.GetUserId())
+	if err != nil {
+		log.Printf("Failed to execute query: %v", err)
+		return &pb.CreateGroupUserResponse{
+			Result: false,
+		}, err
+	}
+
+	return &pb.CreateGroupUserResponse{
+		Result: true,
+	}, nil
 }
 
 func (s *userServer) UpdateGroupUser(ctx context.Context, in *pb.UpdateGroupUserRequest) (*pb.UpdateGroupUserResponse, error) {
+	groupUser := in.GetGroupUserRequest()
 
+	_, err := s.db.Exec("update ignore boards set group_id = ?, user_id = ?", groupUser.GetGroupId(), groupUser.GetUserId())
+	if err != nil {
+		log.Printf("Failed to execute query: %v", err)
+		return &pb.UpdateGroupUserResponse{
+			Result: false,
+		}, err
+	}
+
+	return &pb.UpdateGroupUserResponse{
+		Result: true,
+	}, nil
 }
 
-func (s *userServer) DeleteGroupUser(ctx context.Context, in *pb.DeleteGroupUserRequest) (*pb.DeleteBoardGroupResponse, error) {
+func (s *userServer) DeleteGroupUser(ctx context.Context, in *pb.DeleteGroupUserRequest) (*pb.DeleteGroupUserResponse, error) {
+	id := in.GetId()
 
+	_, err := s.db.Exec("delete from group_users where id = ?", id)
+	if err != nil {
+		log.Printf("Failed to execute query: %v", err)
+		return &pb.DeleteGroupUserResponse{
+			Result: false,
+		}, err
+	}
+
+	return &pb.DeleteGroupUserResponse{
+		Result: true,
+	}, nil
 }
